74fa9ef32044e4a9581c4f081333f3dffd2153ce
Gray.C.Thomas <Gray.C.Thomas@412f3ddc-09fe-11df-9f86-67d497c0387b>
2011-04-11 17:18:30 +0000

    Switched to labview

--- Dave Brain/Robot Comp GVMT/Robot/src/Think.py
+++ Dave Brain/Robot Comp GVMT/Robot/src/Think.py
@@ -1,3 +1,26 @@
-def CheckForStop(sensorDict):
+import math
+def CheckForStop(RobMem):
+    Hit=0;
+    Hit=Hit or RobMem['StreamData']['Bump Right']
+    Hit=Hit or RobMem['StreamData']['Bump Left']
+    Hit=Hit or RobMem['StreamData']['Wheeldrop Left']
+    Hit=Hit or RobMem['StreamData']['Wheeldrop Right']
+    Hit=Hit or RobMem['StreamData']['Wheeldrop Caster']
+    Hit=Hit or RobMem['StreamData']['Wall']
+    Hit=Hit or RobMem['StreamData']['Cliff Left']
+    Hit=Hit or RobMem['StreamData']['Cliff Front Left']
+    Hit=Hit or RobMem['StreamData']['Cliff Front Right']
+    Hit=Hit or RobMem['StreamData']['Cliff Right']
+    RobMem['State']='Impact'
+
+def CheckSpin(RobMem):
+    return RobMem
+
+def UpdateMapSpin(RobMem):
+    RobMem['Pose']=RobMem['Pose']+RobMem['NewAngle(deg)']*math.pi/180
     
-def UpdateMap(sensorDict):
+def UpdateMap(RobMem):
+    RobMem['Loc']=RobMem['Loc']+RobMem['NewDist(mm)']*\
+    [math.cos(RobMem['Pose']),math.sin(RobMem['Pose'])]
+    
+    
